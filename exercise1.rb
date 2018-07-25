d458e3011aba6849c1b197bc4ef86698364e9480
yohanis-ke <yohanes.ke@gmail.com>
2018-07-25 13:58:14 -0400

    exercise1 on OOP-2

---
+++ exercise1.rb
@@ -0,0 +1,54 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  due_date: "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     due_date: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     due_date: "January 1, 2018"
+    },
+    {description: "compile results",
+     due_date: "November 10, 2018"
+    },
+    {description: "version 1",
+     due_date: "January 15, 2019"
+    },
+    {description: "revisions",
+     due_date: "March 30, 2019"
+    },
+    {description: "version 2",
+     due_date: "July 12, 2019"
+    },
+    {description: "final edit",
+     due_date: "October 1, 2019"
+    },
+    {description: "final version",
+     due_date: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     due_date: "December 1, 2019"
+    }
+  ]
+}
+
+
+
+def update(project)
+  count = 0
+  committee_members=project[:committee]
+  steps=project[:steps]
+
+steps.each do |s|
+  s["person"]=   committee_members[count]
+  count +=1
+  if count == 3
+    count=0
+  end
+end
+end
+
+update(project)
+  puts project
